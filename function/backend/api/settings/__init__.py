79d0d5ed6c36e2f02b669b532b110becf610d309
fotox <carsten_stephan@mail.de>
2025-05-02 10:58:17 +0200

    fixed __init__.py import

--- function/backend/api/settings/__init__.py
+++ function/backend/api/settings/__init__.py
@@ -1,10 +1,11 @@
-from . import heating
-from . import photovoltaic
-from . import tanks
-from . import weather
-from . import energy
 from flask import Blueprint
 
 settings_bp = Blueprint("settings", __name__, url_prefix="/api/settings")
 
 # import setting modules
+if True:
+    from . import heating
+    from . import photovoltaic
+    from . import tanks
+    from . import weather
+    from . import energy
