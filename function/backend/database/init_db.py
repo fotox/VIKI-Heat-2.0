5d2985453683247df679ce809f00b279ddb12cb5
fotox <carsten_stephan@mail.de>
2025-05-02 22:38:38 +0200

    update scheme and upgrade init_db

--- function/backend/database/init_db.py
+++ function/backend/database/init_db.py
@@ -7,41 +7,39 @@ def seed_roles():
     existing = Role.query.filter_by(role_name='admin').first()
     if existing:
         print("⚠️ Role 'admin' all ready exists.")
-        return
-
-    admin = Role(
-        id=1,
-        role_name='admin'
-    )
-    db.session.add(admin)
+    else:
+        admin = Role(
+            id=1,
+            role_name='admin'
+        )
+        db.session.add(admin)
 
     # Init user role
     existing = Role.query.filter_by(role_name='user').first()
     if existing:
         print("⚠️ Role 'user' all ready exists.")
-        return
+    else:
+        user = Role(
+            id=2,
+            role_name='user'
+        )
+        db.session.add(user)
 
-    user = Role(
-        id=2,
-        role_name='user'
-    )
-    db.session.add(user)
     db.session.commit()
-    print('✅ User role added')
+    print('✅ User roles added')
 
 
 def seed_users():
     existing = User.query.filter_by(username='viki').first()
     if existing:
         print("⚠️ User 'viki' existiert bereits.")
-        return
-
-    admin = User(
-        id=1,
-        username="viki",
-        role="admin"
-    )
-    admin.set_password("viki")
-    db.session.add(admin)
-    db.session.commit()
-    print("✅ Admin erstellt.")
+    else:
+        admin = User(
+            id=1,
+            username="viki",
+            role="admin"
+        )
+        admin.set_password("viki")
+        db.session.add(admin)
+        db.session.commit()
+        print("✅ Admin erstellt.")
