474c376877c136643475b374bbd1d19513b66030
fotox <carsten_stephan@mail.de>
2025-05-15 00:29:47 +0200

    add connector to fronius inverter

---
+++ function/backend/services/energy/inverter.py
@@ -0,0 +1,52 @@
+from sqlalchemy import select, join
+import requests
+from flask import Response
+
+from extensions import db
+from database.settings import ManufacturerSetting, EnergySetting
+from services.helper import extract_datapoints_from_json_with_api
+
+
+def get_manufacturer_with_energy_settings(manufacturer_id: int):
+    stmt = (
+        select(
+            ManufacturerSetting.url,
+            ManufacturerSetting.api,
+            EnergySetting.ip,
+            EnergySetting.price
+        )
+        .select_from(
+            join(ManufacturerSetting, EnergySetting, ManufacturerSetting.id == EnergySetting.manufacturer, isouter=True)
+        )
+        .where(ManufacturerSetting.id == manufacturer_id)
+        .limit(1)
+    )
+
+    result = db.session.execute(stmt).fetchone()
+    if result:
+        return {
+            "url": result.url,
+            "api": result.api,
+            "ip": result.ip,
+            "price": result.price
+        }
+    else:
+        return None
+
+
+def pull_live_data_from_inverter(manufacturer_id: int):
+    inverter_data: dict = get_manufacturer_with_energy_settings(manufacturer_id)
+
+    if inverter_data is None:
+        return {}
+
+    url: str = f"http://{inverter_data['ip']}{inverter_data['url']}"
+    response: Response = requests.get(url, timeout=5)
+    data: dict = extract_datapoints_from_json_with_api(inverter_data['api'], response.json())
+
+    consume: float = data.get('P_Load') if data.get('P_Load') is not None else 0.0
+    production: float = data.get('P_PV') if data.get('P_PV') is not None else 0.0
+    cover: float = data.get('P_Grid') if data.get('P_Grid') is not None else 0.0
+    accu_capacity: float = data.get('P_Akku') if data.get('P_Akku') is not None else 0.0
+
+    return {'consume': consume, 'production': production, 'cover': cover, 'accu_capacity': accu_capacity}
